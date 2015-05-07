f342796eb4c102478b74dde510019b3bf4d7bf88
Thekgiga <thekgiga@gmail.com>
2015-05-08 01:23:23 +0200

    dodavanje grane

--- SportskaOprema/src/prodavnica/ZaposleniInterface.java
+++
@@ -1,8 +0,0 @@
-package prodavnica;
-
-public interface ZaposleniInterface {
-	
-	public void dodajZaposlenog(String ime, String prezime, String pol, String brTelefona, String Id_zaposlenog, String adresa);
-	public void otpustiZaposlenog(Zaposleni zaposleni);
-
-}
